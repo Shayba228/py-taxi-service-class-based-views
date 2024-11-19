069fb9cfa8ecc877e2577dd23a0a04a6183a1a7e
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-19 02:37:44 +0100

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,45 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-
-from taxi.models import Driver, Car, Manufacturer
+from .models import Driver, Car, Manufacturer
+from django.db.models import Prefetch
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = (Car.objects.select_related("manufacturer")
+                .all().order_by("model"))
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = self.object.cars.select_related("manufacturer")
+        return context
